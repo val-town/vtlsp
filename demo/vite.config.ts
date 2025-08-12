a20b7a5bc001f34f7c5b23fd4ab2ac1c57a03056
Wolf Mermelstein <wolf@404wolf.com>
2025-08-12 11:33:32 -0400

    Remove vite aliases

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,35 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@codemirror/state": path.resolve(
-        __dirname,
-        "node_modules/@codemirror/state",
-      ),
-      "@codemirror/view": path.resolve(
-        __dirname,
-        "node_modules/@codemirror/view",
-      ),
-      "@codemirror/language": path.resolve(
-        __dirname,
-        "node_modules/@codemirror/language",
-      ),
-      "@codemirror/commands": path.resolve(
-        __dirname,
-        "node_modules/@codemirror/commands",
-      ),
-      "@codemirror/autocomplete": path.resolve(
-        __dirname,
-        "node_modules/@codemirror/autocomplete",
-      ),
-      codemirror: path.resolve(__dirname, "node_modules/codemirror"),
-    },
-  },
   optimizeDeps: {
     include: [
       "@codemirror/state",
